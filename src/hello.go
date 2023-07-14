0297f9fa833ae6a0b27435a77635c3ccf25894b1
bsrivastava <bsrivastava@avaya.com>
2023-07-14 13:40:58 +0530

    version

--- src/hello.go
+++ src/hello.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pborman/uuid"
+)
 
 func Hello() string {
+	uuidWithHyphen := uuid.NewRandom()
+	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
+	fmt.Println(uuid)
+
 	return "Hello, world"
+
 }
 
 func main() {
